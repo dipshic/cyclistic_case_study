97c3c55117a87c3b04113a1b1a0992ee1048ca30
dipshic <103956164+dipshic@users.noreply.github.com>
2022-04-19 20:47:38 +0530

    Update cyclistic_analysis.sql

--- cyclistic_analysis.sql
+++ cyclistic_analysis.sql
@@ -1,4 +1,8 @@
--- Drop table
+---------------------------------------------
+-- Data Preparation
+---------------------------------------------
+
+-- Drop tables
 DROP TABLE CYCLIST_TRIP_DATA;
 DROP TABLE MEM_CAS_RIDES;
 DROP TABLE YEAR_RIDES;
@@ -8,7 +12,7 @@ DROP TABLE ROUND_RIDES;
 DROP TABLE MEM_CAS_BIKES_RIDES;
 DROP TABLE BIKES_RIDES;
 
--- checking if all the tables have the same number of columns       
+-- Checking if all the tables have the same number of columns       
 SELECT COUNT(*)
 FROM USER_TAB_COLUMNS
 WHERE table_name = 'TRIP_202004';
@@ -25,7 +29,7 @@ SELECT COUNT(*)
 FROM USER_TAB_COLUMNS
 WHERE table_name = 'TRIP_202103';
 
--- union of all the 12 tables into a single data table
+-- Union of all the 12 tables into a single data table
 CREATE TABLE CYCLIST_TRIP_DATA AS 
 (
     SELECT *
@@ -87,13 +91,13 @@ CREATE TABLE CYCLIST_TRIP_DATA AS
     FROM TRIP_202103
 );
 
--- checking the newly created table 'cyclist_trip_data'
--- SELECT *
--- FROM cyclist_trip_data;
+---------------------------------------------
+-- Data Exploration
+---------------------------------------------
 
--- checking if table exists
--- SELECT table_name
--- FROM USER_TABLES WHERE table_name = 'CYCLIST_TRIP_DATA';
+-- Checking if table exists
+SELECT table_name
+FROM USER_TABLES WHERE table_name = 'CYCLIST_TRIP_DATA';
 
 -- View table
 SELECT *
@@ -104,29 +108,19 @@ SELECT MEMBER_CASUAL, COUNT(*)
 FROM CYCLIST_TRIP_DATA
 GROUP BY MEMBER_CASUAL;
 
--- Check for anomalies in the data
-SELECT DISTINCT MEMBER_CASUAL
-FROM CYCLIST_TRIP_DATA;
-
-SELECT MIN(end_lng),MAX(end_lng),
-       MIN(end_lat),MAX(end_lat), 
-       MIN(start_lng),MAX(start_lng),
-       MIN(start_lat),MAX(start_lat)
-FROM CYCLIST_TRIP_DATA;
-
---counting rides ending at each docking station
+-- Counting rides ending at each docking station
 SELECT END_STATION_ID, END_STATION_NAME, COUNT(1) AS rides
 FROM CYCLIST_TRIP_DATA
 GROUP BY END_STATION_ID, END_STATION_NAME
 ORDER BY rides DESC;
 
---counting rides starting at each docking station
+-- Counting rides starting at each docking station
 SELECT START_STATION_NAME, COUNT(1) AS rides
 FROM CYCLIST_TRIP_DATA
 GROUP BY START_STATION_NAME
 ORDER BY rides DESC;
 
---count number of round trips
+-- Count number of round trips
 SELECT START_STATION_ID, END_STATION_ID,RIDEABLE_TYPE,MEMBER_CASUAL
 FROM CYCLIST_TRIP_DATA
 WHERE START_STATION_ID = END_STATION_ID;
@@ -135,36 +129,51 @@ SELECT COUNT(*)
 FROM CYCLIST_TRIP_DATA
 WHERE START_STATION_ID = END_STATION_ID;
 
---counting total number of trips
+-- Counting total number of trips
 SELECT COUNT(*)
 FROM CYCLIST_TRIP_DATA;
 
---counting number of rideable type
+-- Counting number of rideable type
 SELECT rideable_type, COUNT(1)
 FROM CYCLIST_TRIP_DATA
 GROUP BY rideable_type;
 
---counting ride ids having count >1
+---------------------------------------------
+-- Data Quality Check
+---------------------------------------------
+
+-- See if anything other than member or casual is present in MEMBER_CASUAL
+SELECT DISTINCT MEMBER_CASUAL
+FROM CYCLIST_TRIP_DATA;
+
+-- Check the ranges of latitudes and longitudes
+SELECT MIN(end_lng),MAX(end_lng),
+       MIN(end_lat),MAX(end_lat), 
+       MIN(start_lng),MAX(start_lng),
+       MIN(start_lat),MAX(start_lat)
+FROM CYCLIST_TRIP_DATA;
+
+-- Check if ride ids (which are supposed to be unique) having count >1
 SELECT ride_id, COUNT(1)
 FROM CYCLIST_TRIP_DATA
 GROUP BY ride_id
 HAVING COUNT(1) > 1;
 
---checking for nulls in rows
+-- Checking for nulls in rows
 SELECT *
 FROM CYCLIST_TRIP_DATA
 WHERE started_at IS NULL OR ended_at IS NULL;
 
---checking for rows where column value is absent
---we want to investigate if the blank fields are either due to empty strings, or null , or whitespaces
+-- Checking for rows where column value is absent
+-- We want to investigate if the blank fields are either due to empty strings, or null , or whitespaces
 
---below query is to counter fileds which only have whitespaces 
+-- Below query is to counter fileds which only have whitespaces 
 SELECT COUNT (*) 
 FROM CYCLIST_TRIP_DATA 
 WHERE TRIM(START_STATION_ID) IS NULL OR TRIM(START_STATION_NAME) IS NULL;
 
---visual studio code shows null entries as blank fields.counting number of nulls
---checking on start and end stations
+-- Visual studio code shows null entries as blank fields.counting number of nulls
+-- Checking on start and end stations
 SELECT COUNT (*) 
 FROM CYCLIST_TRIP_DATA 
 WHERE START_STATION_ID IS NULL OR START_STATION_NAME IS NULL;
@@ -172,7 +181,6 @@ WHERE START_STATION_ID IS NULL OR START_STATION_NAME IS NULL;
 SELECT COUNT (*) 
 FROM CYCLIST_TRIP_DATA 
 WHERE END_STATION_ID IS NULL OR END_STATION_NAME IS NULL;
-
 
 SELECT COUNT(*)
 FROM CYCLIST_TRIP_DATA
@@ -190,8 +198,12 @@ SELECT COUNT(*)
 FROM CYCLIST_TRIP_DATA
 WHERE RIDEABLE_TYPE IS NULL;
 
--- delete all rows where any field is null
--- below query deletes 1,95,057 rows
+---------------------------------------------
+-- Data Cleaning
+---------------------------------------------
+
+-- Delete all rows where any field is null
+-- Below query deletes 1,95,057 rows
 DELETE
 FROM CYCLIST_TRIP_DATA
 WHERE RIDE_ID IS NULL
@@ -208,27 +220,29 @@ OR END_LAT IS NULL
 OR END_LNG IS NULL
 OR MEMBER_CASUAL IS NULL;
 
---Identify and exclude data with anomalies
---10743 rows deleted.
+-- Identify and exclude data with anomalies
+-- 10743 rows deleted.
 
 DELETE
 FROM CYCLIST_TRIP_DATA
 WHERE STARTED_AT >= ENDED_AT;
 
---checking if ride ids still have count >1
+-- Checking if ride ids still have count >1
 SELECT ride_id, COUNT(1)
 FROM CYCLIST_TRIP_DATA
 GROUP BY ride_id
 HAVING COUNT(1) > 1;
 
---check again for any nulls
+-- Check again for any nulls
 SELECT COUNT (*) 
 FROM CYCLIST_TRIP_DATA 
 WHERE START_STATION_ID IS NULL OR START_STATION_NAME IS NULL;
 
---Create queries for data visualisations
+---------------------------------------------
+-- Tables for Visualization
+---------------------------------------------
 
---calculate trip length
+-- Calculate trip length
 ALTER TABLE CYCLIST_TRIP_DATA
 ADD trip_duration_secs NUMBER;
 
@@ -236,31 +250,31 @@ ADD trip_duration_secs NUMBER;
 UPDATE CYCLIST_TRIP_DATA
 SET trip_duration_secs = EXTRACT(HOUR FROM (ended_at-started_at))*3600 + EXTRACT(MINUTE FROM (ended_at-started_at))*60 + EXTRACT(SECOND FROM (ended_at-started_at));
 
---number of rides for casual and members
+-- Number of rides for casual and members
 CREATE TABLE MEM_CAS_RIDES AS
     SELECT MEMBER_CASUAL, COUNT(*) AS NO_OF_RIDES
     FROM CYCLIST_TRIP_DATA
     GROUP BY MEMBER_CASUAL
     ORDER BY COUNT(*) DESC;
 
--- member	1936040
--- casual	1347908
+-- Member	1936040
+-- Casual	1347908
 
---count of rides for each bike type
+-- Count of rides for each bike type
 CREATE TABLE BIKES_RIDES AS
     SELECT RIDEABLE_TYPE, COUNT(*) AS NO_OF_RIDES
     FROM CYCLIST_TRIP_DATA
     GROUP BY RIDEABLE_TYPE
     ORDER BY COUNT(*) DESC;
 
---distribution of members and casuals for each bike type
+-- Distribution of members and casuals for each bike type
 CREATE TABLE MEM_CAS_BIKES_RIDES AS
     SELECT RIDEABLE_TYPE, MEMBER_CASUAL, COUNT(*) AS NO_OF_RIDES
     FROM CYCLIST_TRIP_DATA
     GROUP BY RIDEABLE_TYPE,MEMBER_CASUAL
     ORDER BY RIDEABLE_TYPE ASC, COUNT(*) DESC;
 
---count round trips for each bike type and membership type
+-- Count round trips for each bike type and membership type
 CREATE TABLE ROUND_RIDES AS
     SELECT START_STATION_NAME, COUNT(*) AS NO_OF_ROUND_TRIPS, RIDEABLE_TYPE, MEMBER_CASUAL
     FROM CYCLIST_TRIP_DATA
@@ -268,7 +282,7 @@ CREATE TABLE ROUND_RIDES AS
     GROUP BY START_STATION_NAME, RIDEABLE_TYPE, MEMBER_CASUAL
     ORDER BY START_STATION_NAME, COUNT(*) DESC, RIDEABLE_TYPE;
 
---distribution of casual and member rides across the year
+-- Distribution of casual and member rides across the year
 CREATE TABLE YEAR_RIDES AS
     SELECT TO_CHAR(STARTED_AT,'MON-YYYY') AS MON_YEAR, MEMBER_CASUAL, COUNT(*) AS NO_OF_RIDES
     FROM CYCLIST_TRIP_DATA
